af13127e32a2782a270b8da6c1c7cf0ade924fd8
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-24 14:37:18 +0530

    express router setup

---
+++ routes/ContactRouter.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create Contacts" });
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact Details for ${req.params.id}` });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+});
+
+module.exports = router;
