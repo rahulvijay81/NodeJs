bb86d18cadf054f3c7c37f8a7079262fe902d9dc
rahulvijay81 <118264222+rahulvijay81@users.noreply.github.com>
2024-04-24 14:22:52 +0530

    node project setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express')
+const express = require("express");
+const dotenv = require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const port = 5000 ;
+const port = process.env.PORT || 5000;
 
-app.listen(port , () => {
-    console.log(`server running on port ${port}`);
-})
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
